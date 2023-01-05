0ee67788232040b9d8beaac29a35f9d9cc2aaf6e
S-I-M-O-N <S-I-M-O-N@gmx.li>
2023-01-05 09:03:21 +0100

    Bugfixes and optimization of space allocation.

--- gb-multi-rom-creator.py
+++ gb-multi-rom-creator.py
@@ -56,24 +56,36 @@ elif platform == "win32":
     # windows
     clear = lambda: os.system('cls')
 
-rom1=""
-rom2=""
-rom3=""
+
 romno=1
+rom0no=1
+rom1no=0
+rom2no=0
+rom3no=0
 romsselectedsize=0
 chipsize=2048
-maxromsize=256
+maxromsize=chipsize/4
+erasesize=maxromsize/2
 startupsize=32
+cart0free=(chipsize/4)-startupsize
+cart1free=(chipsize/4)-erasesize
+cart2free=(chipsize/4)
+cart3free=(chipsize/4)
 blank=os.path.join("res","blank.gb")
 startup=os.path.join("res","startup22.gb")
 mode=2
-programversion="GB Multi Rom Creator v2.0"
+programversion="GB Multi Rom Creator v2.1"
 
 def select_mode():
     global chipsize
     global startup
     global maxromsize
     global startupsize
+    global erasesize
+    global cart0free
+    global cart1free
+    global cart2free
+    global cart3free
     global mode
     while True:
         clear()
@@ -100,8 +112,10 @@ def select_mode():
             elif(item2==1):
                 startup=os.path.join("res","startup.gb")
                 startupsize=os.path.getsize(startup)/1024
+                cart0free=0
+                cart1free=maxromsize
+                erasesize=0
                 mode=1
-                maxromsize=512
             elif(item2==2):
                 startup=os.path.join("res","startup22.gb")
                 startupsize=os.path.getsize(startup)/1024
@@ -125,42 +139,63 @@ def select_mode():
         if(item==0):
             break
         elif(item==1):
+            confirmation = "n"
             clear()
-            print("1: 2048kB (AM29F016, etc.)")
-            print("2: 1024kB (AM29F080, etc.)")
-            print("3:  512kB (AM29F040, etc.)")
-            print()
-            print( "Please select menu entry. Enter 0 to exit menu.")
-            item2=int(input())
-            if(item2==0):
-                break
-            elif(item2==1):
-                chipsize=2048
-                maxromsize=256
+            while confirmation != "y":
+                print("1: 2048kB (AM29F016, etc.)")
+                print("2: 1024kB (AM29F080, etc.)")
+                print("3:  512kB (AM29F040, etc.)")
+                print()
+                print( "Please select menu entry. Enter 0 to exit menu.")
+                item2=int(input())
+                if(item2==0):
+                    confirmation = "y"
+                elif(item2==1):
+                    chipsize=2048
+                    confirmation = "y"
+                elif(item2==2):
+                    chipsize=1024
+                    confirmation = "y"
+                elif(item2==3):
+                    chipsize=512
+                    confirmation = "y"
+                else:
+                    print( "Wrong selection: Select any number from 0-3")
+                
+                maxromsize=chipsize/4
+                erasesize=maxromsize/2
+                cart0free=maxromsize-startupsize
                 if mode==1:
-                    maxromsize=512
-                break
-            elif(item2==2):
-                chipsize=1024
-                maxromsize=128
-                break
-            elif(item2==3):
-                chipsize=512
-                maxromsize=64
-                break
-            else:
-                print( "Wrong selection: Select any number from 0-3")
-                break
+                    cart0free=0
+                cart1free=maxromsize-erasesize
+                if mode==1:
+                    cart1free=maxromsize
+                cart2free=maxromsize
+                cart3free=maxromsize
+            break
 
 
 def select_rom():
     global romno
+    global rom0no
+    global rom1no
+    global rom2no
+    global rom3no
     global confirmation
-    global romsselected
+    global romsselected0
+    global romsselected1
+    global romsselected2
+    global romsselected3
     global romsselectedsize
     global maxromsize
     global chipsize
     global startupsize
+    global erasesize
+    global cart0free
+    global cart1free
+    global cart2free
+    global cart3free
+    
     print("Available roms:")
     i=1
     while True:
@@ -175,13 +210,38 @@ def select_rom():
         elif(item<=len(roms)+1):
             if(os.path.getsize(roms[item-1])>maxromsize*1024):
                 print("The rom is larger than",maxromsize,"kB and thus not compatible. Please reselect.")
-            elif(os.path.getsize(roms[item-1])>(chipsize-startupsize-maxromsize-(romsselectedsize/1024))*1024):
-                print("The rom is larger than the remaining free space. Please reselect.")
-            else:
-                romsselected.append(roms[item-1])
-                romsselectedsize=romsselectedsize+os.path.getsize(roms[item-1])
+            elif(os.path.getsize(roms[item-1])<=cart0free*1024):
+                romsselected0.append(roms[item-1])
+                cart0free=cart0free-(os.path.getsize(roms[item-1])/1024)
+                rom0no += 1
                 romno += 1
                 break
+            elif(os.path.getsize(roms[item-1])<=cart1free*1024):
+                romsselected1.append(roms[item-1])
+                cart1free=cart1free-(os.path.getsize(roms[item-1])/1024)
+                if mode==1:
+                    cart1free=0
+                rom1no += 1
+                romno += 1
+                break
+            elif(os.path.getsize(roms[item-1])<=cart2free*1024):
+                romsselected2.append(roms[item-1])
+                cart2free=cart2free-(os.path.getsize(roms[item-1])/1024)
+                if mode==1:
+                    cart2free=0
+                rom2no += 1
+                romno += 1
+                break
+            elif(os.path.getsize(roms[item-1])<=cart3free*1024):
+                romsselected3.append(roms[item-1])
+                cart3free=cart3free-(os.path.getsize(roms[item-1])/1024)
+                if mode==1:
+                    cart3free=0
+                rom3no += 1
+                romno += 1
+                break
+            else:
+                print("The rom is larger than the remaining free space. Please reselect.")
         else:
             print( "Wrong selection: Select any number from 1-",len(roms))
 
@@ -196,7 +256,10 @@ confirmation="n"
 clear()
 select_mode()
 
-romsselected=[startup]
+romsselected0=[startup]
+romsselected1=[]
+romsselected2=[]
+romsselected3=[]
 confirmation = "n"
 while confirmation != "y": 
     i=1
@@ -205,14 +268,24 @@ while confirmation != "y":
     print("Startup Menu:",startup)
     print("Chip size:",chipsize,"kB")
     print("Max rom size:",maxromsize,"kB")
-    if mode==2:
-        print("Size menu and selected roms:",int(startupsize+(romsselectedsize/1024)),"kB")
-        print("Remaining space for additional roms:",int(chipsize-startupsize-maxromsize-(romsselectedsize/1024)),"kB")
-        print("Temp r/w memory between",chipsize/4,"kB and",chipsize/4+maxromsize,"kB") 
+    print("Remaining cart space for additional roms:") #,int(chipsize-startupsize-maxromsize-(romsselectedsize/1024)),"kB")
+    print("#0[",int(cart0free),"kB ] ","#1[",int(cart1free),"kB ] ","#2[",int(cart2free),"kB ] ","#3[",int(cart3free),"kB ] ")
     print("")
     print("Selected roms:")
-    while i < len(romsselected):
-        print(i,":",romsselected[i])
+    while i < len(romsselected0):
+        print(i,":",romsselected0[i])
+        i += 1
+    i=0
+    while i < len(romsselected1):
+        print(i+len(romsselected0),":",romsselected1[i])
+        i += 1
+    i=0
+    while i < len(romsselected2):
+        print(i+len(romsselected0)+len(romsselected1),":",romsselected2[i])
+        i += 1
+    i=0
+    while i < len(romsselected3):
+        print(i+len(romsselected0)+len(romsselected1)+len(romsselected2),":",romsselected3[i])
         i += 1
     print("")
     select_rom()
@@ -224,8 +297,20 @@ while confirmation != "y":
     print("Max rom size:",maxromsize,"kB")
     print("")
     print("Selected roms:")
-    while i < len(romsselected):
-        print(i,":",romsselected[i])
+    while i < len(romsselected0):
+        print(i,":",romsselected0[i])
+        i += 1
+    i=0
+    while i < len(romsselected1):
+        print(i+len(romsselected0),":",romsselected1[i])
+        i += 1
+    i=0
+    while i < len(romsselected2):
+        print(i+len(romsselected0)+len(romsselected1),":",romsselected2[i])
+        i += 1
+    i=0
+    while i < len(romsselected3):
+        print(i+len(romsselected0)+len(romsselected1)+len(romsselected2),":",romsselected3[i])
         i += 1
     print("")
     if mode==1:
@@ -233,26 +318,21 @@ while confirmation != "y":
             break
             
   
-romno=0
+rom0no=0
+rom1no=0
+rom2no=0
+rom3no=0
 romsselectedsize=0
 fo=open("multirom.gb", 'w')
 fo.close()
 fo=open("multirom.gb", 'ab')
 
-while romno < len(romsselected):
+while rom0no < len(romsselected0):
     numBytes=0
 
-    romsize=(os.path.getsize(romsselected[romno]))
-    if mode==2:
-        if int(romsselectedsize)<int(chipsize*1024/4):
-            if int(romsize)>(int(chipsize*1024/4))-int(romsselectedsize):
-                pad=int(chipsize*1024/4)-int(romsselectedsize)
-                while (numBytes<pad+(maxromsize*1024)):
-                    fo.write(b"\00")
-                    numBytes=numBytes+1
-                numBytes=0
+    romsize=(os.path.getsize(romsselected0[rom0no]))
                 
-    fi=open(romsselected[romno], 'rb')
+    fi=open(romsselected0[rom0no], 'rb')
     while (numBytes<romsize):
         fo.write(fi.read(1))
         numBytes=numBytes+1
@@ -261,15 +341,73 @@ while romno < len(romsselected):
             fo.write(b"\00")
             numBytes=numBytes+1
     fi.close()
-    fo.close()
-    romsselectedsize=os.path.getsize("multirom.gb")
-    fo=open("multirom.gb", 'ab')
-    romno += 1
+    rom0no += 1
 
-fo.close()
-numBytes=os.path.getsize("multirom.gb")
-fo=open("multirom.gb", 'ab')
-while (numBytes<(chipsize*1024)):
+numBytes=int(maxromsize-cart0free)*1024
+while (numBytes<(maxromsize+erasesize)*1024):
+    fo.write(b"\00")
+    numBytes=numBytes+1
+
+while rom1no < len(romsselected1):
+    numBytes=0
+
+    romsize=(os.path.getsize(romsselected1[rom1no]))
+                
+    fi=open(romsselected1[rom1no], 'rb')
+    while (numBytes<romsize):
+        fo.write(fi.read(1))
+        numBytes=numBytes+1
+    if mode==1:
+        while (numBytes<(maxromsize)*1024):
+            fo.write(b"\00")
+            numBytes=numBytes+1
+    fi.close()
+    rom1no += 1
+
+numBytes=int(maxromsize-cart1free)*1024
+while (numBytes<maxromsize*1024):
+    fo.write(b"\00")
+    numBytes=numBytes+1
+
+while rom2no < len(romsselected2):
+    numBytes=0
+
+    romsize=(os.path.getsize(romsselected2[rom2no]))
+                
+    fi=open(romsselected2[rom2no], 'rb')
+    while (numBytes<romsize):
+        fo.write(fi.read(1))
+        numBytes=numBytes+1
+    if mode==1:
+        while (numBytes<maxromsize*1024):
+            fo.write(b"\00")
+            numBytes=numBytes+1
+    fi.close()
+    rom2no += 1
+
+numBytes=int(maxromsize-cart2free)*1024
+while (numBytes<maxromsize*1024):
+    fo.write(b"\00")
+    numBytes=numBytes+1
+
+while rom3no < len(romsselected3):
+    numBytes=0
+
+    romsize=(os.path.getsize(romsselected3[rom3no]))
+                
+    fi=open(romsselected3[rom3no], 'rb')
+    while (numBytes<romsize):
+        fo.write(fi.read(1))
+        numBytes=numBytes+1
+    if mode==1:
+        while (numBytes<maxromsize*1024):
+            fo.write(b"\00")
+            numBytes=numBytes+1
+    fi.close()
+    rom3no += 1
+
+numBytes=int(maxromsize-cart3free)*1024
+while (numBytes<int(maxromsize*1024)):
     fo.write(b"\00")
     numBytes=numBytes+1
 
